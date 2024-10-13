489e6027d7260910bc008431220d19adc0a13a41
Khalil Bel <wayfay2222@gmail.com>
2024-10-13 12:36:42 +0100

    edit testcase file

--- tests/FunctionsTests.php
+++ tests/FunctionsTests.php
@@ -1,6 +1,8 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+require_once __DIR__ . '/../src/functions.php'; 
+
 class FunctionsTests extends TestCase {
     public function testAdd() {
         $this->assertEquals(3, add(1, 2));
