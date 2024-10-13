4a4216f373fc746ade8efc6789d9e7fc924d181f
Khalil Bel <wayfay2222@gmail.com>
2024-10-13 15:22:17 +0100

    add a new code structure rules

--- tests/FunctionsTests.php
+++ tests/FunctionsTests.php
@@ -4,11 +4,10 @@ namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ . '/../src/functions.php';
-
 class FunctionsTests extends TestCase
 {
-    public function testAdd() {
+    public function testAdd()
+    {
         $this->assertEquals(3, add(1, 2));
         $this->assertEquals(10, add(5, 5));
     }
